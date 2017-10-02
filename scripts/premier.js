e27f7bba4eaf6a51a31f2e3556c412a9ac18f79a
toure <tourekadiy@gmail.com>
2017-10-02 11:59:08 +0200

    tests_unitaires with functions;

--- scripts/premier.js
+++ scripts/premier.js
@@ -1,5 +1,10 @@
 function additionner(a, b) {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        throw new Error('Expect numbers as parameters');
+    }
     return a + b;
-}
 
-function soustraire
+    function soustraire(a, b) {
+        return a - b;
+    }
+}
--- scripts/premier.spec.js
+++ scripts/premier.spec.js
@@ -2,13 +2,24 @@ it('should be true', () => {
     expect(true).toBe(true);
 });
 describe('', ('function additionner', () => {
-    it('should add 2 to 2', () => {
-        let resultat = additionner(2, 2);
-        expect(resultat).toBe(4);
-    });
+        it('should add 2 to 2', () => {
+            let resultat = additionner(2, 2);
+            expect(resultat).toBe(4);
+        });
 
-    //faire test avec nombres négatifs
+        //faire test avec nombres négatifs
 
+        it('should add negatives numbers', () => {
+            let resultat = additionner(-2, -2);
+            expect(resultat).toBe(-4);
+        });
+    }))
     //faire test avec chaînes de caractère
+    //it('should concatenate string params', () => {
+    //expect(additionner('bloup', 2)).toBe('bloup2');
+    //});
 
-}));
+it('should throw error with string params', () => {
+    expect(additionner.bind(null, 'bloup', 2))
+        .toThrow(new Error('Expect numbers as parameters'));
+});
