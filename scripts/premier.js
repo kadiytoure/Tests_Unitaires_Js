a4d3fa4a97798390a5aa1828d285730c0282e757
toure <tourekadiy@gmail.com>
2017-10-02 11:28:22 +0200

    first commit;

---
+++ scripts/premier.js
@@ -0,0 +1,5 @@
+function additionner(a, b) {
+    return a + b;
+}
+
+function soustraire
---
+++ scripts/premier.spec.js
@@ -0,0 +1,14 @@
+it('should be true', () => {
+    expect(true).toBe(true);
+});
+describe('', ('function additionner', () => {
+    it('should add 2 to 2', () => {
+        let resultat = additionner(2, 2);
+        expect(resultat).toBe(4);
+    });
+
+    //faire test avec nombres négatifs
+
+    //faire test avec chaînes de caractère
+
+}));
